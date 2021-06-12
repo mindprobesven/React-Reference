58257330c9d97d17f064988da19eb183546183fc
Sven Kohn <sven@mindprobe.io>
2021-06-12 16:31:01 +0200

    add: React reference

--- React - 2021/Techniques/Images/Responsive parallax image scroll with intersect/src/components/Navigation.jsx
+++ React - 2021/Techniques/Images/Responsive parallax image scroll with intersect/src/components/Navigation.jsx
@@ -8,16 +8,16 @@ const Navigation = () => {
   return (
     <div className="navigation">
       <div className="navigation__link-container">
-        <a href="/home" className="navigation__link-text">Home</a>
+        <a href="/home" className="navigation__link">HOME</a>
       </div>
       <div className="navigation__link-container">
-        <a href="/home" className="navigation__link-text">Household</a>
+        <a href="/home" className="navigation__link">CATALOG</a>
       </div>
       <div className="navigation__link-container">
-        <a href="/home" className="navigation__link-text">Electronics</a>
+        <a href="/home" className="navigation__link">OFFERS</a>
       </div>
       <div className="navigation__link-container">
-        <a href="/home" className="navigation__link-text">Kids</a>
+        <a href="/home" className="navigation__link">CONTACTS</a>
       </div>
     </div>
   );
