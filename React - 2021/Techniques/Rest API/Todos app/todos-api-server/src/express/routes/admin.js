52ef1da51be10ee8c3196afd46f39c7672fa5ecc
Sven Kohn <sven@mindprobe.io>
2021-07-01 15:58:14 +0200

    add: React reference

--- React - 2021/Techniques/Rest API/Todos app/todos-api-server/src/express/routes/admin.js
+++ React - 2021/Techniques/Rest API/Todos app/todos-api-server/src/express/routes/admin.js
@@ -1,5 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable consistent-return */
+/* eslint-disable no-unused-vars */
 /* eslint-disable max-len */
 const express = require('express');
+const validator = require('validator');
+const {
+  body, validationResult, ValidationChain, checkSchema,
+} = require('express-validator');
+
 const userModelExport = require('../../mongo/schemas/user');
 const logger = require('../../utils/logger');
 
@@ -7,9 +15,93 @@ const adminRouter = express.Router();
 
 adminRouter.use(express.json());
 
-// curl -X POST --data '{"user":"mindprobesven", "firstName":"Sven", "lastName":"Kohn"}' -H "Accept: application/json" -H "Content-Type: application/json" http://127.0.0.1:5000/admin/user/add
-adminRouter.post('/user/add', async (req, res) => {
-  const allowedContentType = 'application/json';
+const validateUser = (validations) => async (req, res, next) => {
+  // await Promise.all(validations.map((validation) => validation.run(req)));
+  await validations.run(req);
+
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+
+  res.status(400).json({ errors: errors.array() });
+};
+
+// curl -X POST --data '{"firstName":" Sveñ än!3", "lastName":"Kohn", "email":" sVen@Mind-probe.com"}' -H "Accept: application/json" -H "Content-Type: application/json" http://127.0.0.1:5000/admin/user/add
+// curl -X POST --data '{"firstName":" Sveñ ü", "lastName":"Kohn", "email":" sVen@Mind-probe.com"}' -H "Accept: application/json" -H "Content-Type: application/json" http://127.0.0.1:5000/admin/user/add
+// curl -X POST --data '{"firstName":" Sveä", "lastName":"Kohn", "email":" sVen@Mind-probe.com"}' -H "Accept: application/json" -H "Content-Type: application/json" http://127.0.0.1:5000/admin/user/add
+// curl -X POST --data '{"firstName":" Sven", "lastName":"Kohn", "email":" sVen@Mind-probe.com"}' -H "Accept: application/json" -H "Content-Type: application/json" http://127.0.0.1:5000/admin/user/add
+// curl -X POST --data '{"firstName":"", "lastName":"Kohn", "email":" sVen@Mind-probe.com"}' -H "Accept: application/json" -H "Content-Type: application/json" http://127.0.0.1:5000/admin/user/add
+adminRouter.post(
+  '/user/add',
+  validateUser(
+    checkSchema({
+      email: {
+        trim: true,
+        notEmpty: {
+          errorMessage: 'Email address not specified',
+          bail: true,
+        },
+        isLength: {
+          options: { max: 50 },
+          errorMessage: 'Email address is too long',
+          bail: true,
+        },
+        isEmail: {
+          errorMessage: 'Invalid email address',
+        },
+        normalizeEmail: true,
+      },
+      firstName: {
+        trim: true,
+        notEmpty: {
+          errorMessage: 'Not specified',
+          bail: true,
+        },
+        isLength: {
+          options: { max: 50 },
+          errorMessage: 'Too long',
+          bail: true,
+        },
+        /* isAlpha: {
+          options: ['en-US', { ignore: ' -' }],
+          errorMessage: 'Invalid characters',
+        }, */
+        custom: {
+          options: (value) => {
+            console.log(`Options custom ${value}`);
+            console.log(validator.isAlpha(value, 'en-US', { ignore: ' -' }));
+            console.log(validator.isAlpha(value, 'es-ES', { ignore: ' -' }));
+            console.log(validator.isAlpha(value, 'de-DE', { ignore: ' -' }));
+
+            if (validator.isAlpha(value, 'en-US', { ignore: ' -' })
+            || validator.isAlpha(value, 'es-ES', { ignore: ' -' })
+            || validator.isAlpha(value, 'de-DE', { ignore: ' -' })) {
+              return value;
+            }
+            throw new Error('Invalid characters');
+          },
+        },
+      },
+    }),
+  ),
+  async (req, res) => {
+    console.log(req.body);
+    res.sendStatus(200);
+
+    /* try {
+    const savedUserDoc = await userModelExport.addUser(sampleData);
+    console.log(savedUserDoc);
+    res.sendStatus(200);
+  } catch (error) {
+    logger.mongo.log({
+      level: 'error',
+      message: `Schema validation failed - ${error.message} - ${req.originalUrl} - ${req.ip} - ${req.get('user-agent')}`,
+    });
+    res.status(403).send('403 - Validation Failed');
+  } */
+
+  /* const allowedContentType = 'application/json';
 
   if (req.get('Content-Type') === allowedContentType
   && req.is('application/json') === allowedContentType
@@ -32,8 +124,9 @@ adminRouter.post('/user/add', async (req, res) => {
     });
 
     res.status(406).send('406 - Not Acceptable');
-  }
-});
+  } */
+  },
+);
 
 adminRouter.get('/', (req, res) => {
   res.json({ username: 'mindprobesven' });
--- React - 2021/Techniques/Rest API/Todos app/todos-api-server/src/mongo/schemas/user.js
+++ React - 2021/Techniques/Rest API/Todos app/todos-api-server/src/mongo/schemas/user.js
@@ -1,24 +1,12 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
-  user: {
-    type: String,
-    required: true,
-    trim: true,
-    minLength: 1,
-    maxLength: 50,
-    unique: true,
-  },
   firstName: {
     type: String,
     required: true,
     trim: true,
     minLength: 1,
     maxLength: 50,
-    validate: {
-      validator: (value) => !/\d/.test(value),
-      message: () => 'Must not contain a number',
-    },
   },
   lastName: {
     type: String,
@@ -26,10 +14,14 @@ const userSchema = new Schema({
     trim: true,
     minLength: 1,
     maxLength: 50,
-    validate: {
-      validator: (value) => !/\d/.test(value),
-      message: () => 'Must not contain a number',
-    },
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 1,
+    maxLength: 100,
+    unique: true,
   },
 }, {
   timestamps: true,
